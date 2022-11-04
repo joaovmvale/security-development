cb37094ab41aed647c6eaca0f4c48a9c94b260a0
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2022-11-03 21:32:27 -0300

    feat(rabbitmq): add rabbitMQ

--- src/main/java/com/securityDevelopment/SecurityDevelopmentApplication.java
+++ src/main/java/com/securityDevelopment/SecurityDevelopmentApplication.java
@@ -1,13 +1,15 @@
 package com.securityDevelopment;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class SecurityDevelopmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityDevelopmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDevelopmentApplication.class, args);
+    }
 
 }
--- src/main/java/com/securityDevelopment/equipment/controller/EquipmentController.java
+++ src/main/java/com/securityDevelopment/equipment/controller/EquipmentController.java
@@ -4,8 +4,10 @@ import com.securityDevelopment.equipment.model.EquipmentModel;
 import com.securityDevelopment.equipment.model.dto.EquipmentDTO;
 import com.securityDevelopment.equipment.model.dto.EquipmentResponseDTO;
 import com.securityDevelopment.equipment.service.EquipmentService;
+import com.securityDevelopment.rabbitMQ.QueueSender;
 import com.securityDevelopment.utils.exception.CustomException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +21,18 @@ import java.util.stream.Collectors;
 @SecurityRequirement(name = "Bearer Authentication")
 public class EquipmentController {
     EquipmentService equipmentService;
-
     @Autowired
-    public EquipmentController(EquipmentService equipmentService) {
+    public EquipmentController(EquipmentService equipmentService, AmqpTemplate queueSender) {
         this.equipmentService = equipmentService;
+        this.queueSender = queueSender;
     }
+    private final AmqpTemplate queueSender;
 
     @PostMapping
     public ResponseEntity<EquipmentResponseDTO> create(@RequestBody EquipmentDTO dto) throws CustomException {
         EquipmentModel equipment = equipmentService.create(dto.transformToEquipmentModel());
         EquipmentResponseDTO response = EquipmentResponseDTO.transformToEquipmentResponseDTO(equipment);
+        queueSender.convertAndSend("securityDev-exchange", "securityDev-routing-key", "Create equipments");
         return ResponseEntity.ok(response);
     }
 
@@ -36,6 +40,7 @@ public class EquipmentController {
     public ResponseEntity<List<EquipmentResponseDTO>> listAll() {
         List<EquipmentModel> equipment = equipmentService.listAll();
         List<EquipmentResponseDTO> response = equipment.stream().map(EquipmentResponseDTO::transformToEquipmentResponseDTO).collect(Collectors.toList());
+        queueSender.convertAndSend("securityDev-exchange", "securityDev-routing-key", "List equipments");
         return ResponseEntity.ok(response);
     }
 
@@ -43,6 +48,7 @@ public class EquipmentController {
     public ResponseEntity<EquipmentResponseDTO> findById(@PathVariable UUID id) {
         EquipmentModel equipment = equipmentService.getById(id);
         EquipmentResponseDTO response = EquipmentResponseDTO.transformToEquipmentResponseDTO(equipment);
+        queueSender.convertAndSend("securityDev-exchange", "securityDev-routing-key", "List by id equipments");
         return ResponseEntity.ok(response);
     }
 
@@ -57,12 +63,14 @@ public class EquipmentController {
 
         equipmentService.create(equipment);
         EquipmentResponseDTO response = EquipmentResponseDTO.transformToEquipmentResponseDTO(equipment);
+        queueSender.convertAndSend("securityDev-exchange", "securityDev-routing-key", "Edit equipments");
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         equipmentService.deleteById(id);
+        queueSender.convertAndSend("securityDev-exchange", "securityDev-routing-key", "Delete equipments");
         return ResponseEntity.noContent().build();
     }
 
